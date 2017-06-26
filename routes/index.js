70efdd2cc1d5d5997a94bbf895b8b36244127b18
evercx <306534137@qq.com>
2017-06-26 22:54:41 +0800

    添加 HTTPS服务器

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello World!',
-    subtitle: 'You have succeeded in running this web service.'
-  })
+
+  if (ctx.protocol === "https"){
+    await ctx.render('index', {
+      title: 'Hello World!(HTTPS)',
+      subtitle: 'You have succeeded in running this web service.'
+    })
+  }else{
+    await ctx.render('index', {
+      title: 'Hello World!(HTTP)',
+      subtitle: 'You have succeeded in running this web service.'
+    })
+  }
+
 })
 
 router.get('/string', async (ctx, next) => {
